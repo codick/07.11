291f506bfb69d689629ae4c70d3e42c5bab8a4f4
codick <114742863+codick@users.noreply.github.com>
2022-11-07 15:28:43 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+// Задание №5
+
+// function go(){
+//     elem = document.getElementById('test');
+//     alert(elem.value);
+// }
+
+// №6
+
+// function go(){
+//     elem = document.getElementById('test');
+//     test.value = 'Ой, я поменял свой текст!'
+// }
+
+// №7
+
+function go(){
+    elem = document.getElementById('test');
+    elem.src = 'assets/img/2.jpg';
+}
+
+
+
+
+
+
+
